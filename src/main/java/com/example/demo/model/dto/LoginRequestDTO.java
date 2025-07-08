113d4939b797a1c148dab16060b09ab835104264
LucioSans <lucianosantos2476@gmail.com>
2025-07-08 12:02:33 -0300

    Update LoginRequestDTO.java

--- src/main/java/com/example/demo/model/dto/LoginRequestDTO.java
+++ src/main/java/com/example/demo/model/dto/LoginRequestDTO.java
@@ -1,14 +1,17 @@
 package com.example.demo.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequestDTO(String email, Boolean senha) {
 
     @NotNull
+    @NotBlank
     public String getEmail() {
         return email;
     }
     @NotNull
+    @NotBlank
     public Boolean getSenha() {
         return senha;
     }
