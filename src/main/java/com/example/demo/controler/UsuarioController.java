faf43885a5ade84a6e2b82546c0861edf391e2de
LucioSans <lucianosantos2476@gmail.com>
2025-07-08 11:58:05 -0300

    feat: correcao do login

--- src/main/java/com/example/demo/controler/UsuarioController.java
+++ src/main/java/com/example/demo/controler/UsuarioController.java
@@ -2,6 +2,7 @@ package com.example.demo.controler;
 import com.example.demo.model.dto.LoginRequestDTO;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +37,7 @@ public class UsuarioController {
 
     @PostMapping("/login")
     @CrossOrigin(origins = "*")
-    public ResponseEntity<LoginRequestDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+    public ResponseEntity<LoginRequestDTO> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
         boolean autenticado = usuarioService.autenticar(loginRequestDTO.getEmail(), String.valueOf(loginRequestDTO.getSenha()));
         if (autenticado) {
             // Agora, você INSTANCIA um objeto LoginResponseDTO para o sucesso
