f570bab0e09fe3b639f2643a30c0f81f13c57a95
LucioSans <lucianosantos2476@gmail.com>
2025-07-09 15:02:34 -0300

    feat: correção de conflitos do cors

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -7,15 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    public void addCorsMapping(CorsRegistry registry){
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:5500",
                         "http://localhost:5500"
                 )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Permite os métodos HTTP comuns
-                .allowedHeaders("*") // Permite todos os cabeçalhos (Authorization, Content-Type, etc.)
-                .allowCredentials(true) // Se você usa cookies ou tokens JWT no cabeçalho Authorization
-                .maxAge(3600); // Tempo para o navegador armazenar em cache a resposta do preflight (1 hora)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
