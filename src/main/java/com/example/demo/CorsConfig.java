29d42545b0a1500859a0e248b5fcce3a7101af52
LucioSans <lucianosantos2476@gmail.com>
2025-08-05 13:27:45 -0300

    feat: mudança no cors

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -12,7 +12,9 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://127.0.0.1:5500",
-                        "http://localhost:5500"
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5501",
+                        "http://localhost:5501"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
