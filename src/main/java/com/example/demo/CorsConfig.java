662031ed34dbc2c5c62946007ca73fb323bf2304
LucioSans <lucianosantos2476@gmail.com>
2025-08-11 12:56:54 -0300

    feat: correção CORS

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://127.0.0.1:5500",
                         "http://localhost:5500",
                         "http://127.0.0.1:5501",
-                        "http://localhost:5501"
+                        "http://localhost:5501",
+                        "https://needuk-puce.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
